083a36820904cd09b3122036a0b8f29de5cc756a
Greg Munro <gregmunro90@gmail.com>
2022-03-08 14:44:33 +0100

    img swapped from cardMedia

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Typography, Button, Grid } from "@material-ui/core";
 import useStyles from "./styles";
+import CartItem from './CartItem/CartItem'
 
 const Cart = ({ cart }) => {
   const classes = useStyles();
@@ -16,7 +17,7 @@ const Cart = ({ cart }) => {
       <Grid container spacing={3}>
         {cart.line_items.map((item) => (
           <Grid item xs={12} sm={4} key={item.id}>
-            <div>{item.name}</div>
+            <CartItem item={item} />
           </Grid>
         ))}
       </Grid>
--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@material-ui/core'
+import React from "react";
+import {
+  Typography,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+} from "@material-ui/core";
 
-import useStyles from './styles'
+import useStyles from "./styles";
 
-const CartItem = () => {
+const CartItem = ({ item }) => {
+  const classes = useStyles();
+
   return (
-    <div>CartItem</div>
-  )
-}
+    <Card>
+      <img src={item.image.url} alt={item.name} className={classes.media} />
+      <CardContent className={classes.cardContent}>
+        <Typography variant="h4">{item.name}</Typography>
+        <Typography variant="h5">
+          {item.line_total.formatted_with_symbol}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.CardActions}>
+        <div className={classes.button}>
+          <Button type="button" size="small">
+            -
+          </Button>
+          <Typography>{item.quantity}</Typography>
+          <Button type="button" size="small">
+            +
+          </Button>
+        </div>
+        <Button variant="contained" type="button" color="secondary">
+          Remove
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
 
-export default CartItem
+export default CartItem;
